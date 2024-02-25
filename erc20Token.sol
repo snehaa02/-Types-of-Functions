f7e2c466a8b0f9784ac71c9c09d9553a43f51f9c
Sneha Jha <91584879+snehaa02@users.noreply.github.com>
2024-02-25 18:07:15 +0530

    Update erc20Token.sol

--- erc20Token.sol
+++ erc20Token.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor() ERC20("MyToken", "MTK") {
+        owner = msg.sender;
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
